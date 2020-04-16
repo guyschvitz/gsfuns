6818988d86650329e50c214c71c5d268c62d8970
guyschvitz <guy.schvitz@gess.ethz.ch>
2020-04-16 16:58:30 +0200

    Fix function plotGroupedNA

--- R/plotGroupedNA.R
+++ R/plotGroupedNA.R
@@ -16,11 +16,11 @@ plotGroupedNA <- function(data, groupvar){
     group_by(!!q.groupvar) %>%
     dplyr::summarize_all(shareNA) %>%
     melt(id.vars = quo_name(q.groupvar))
-  p <- ggplot(na.share, aes(x=q.groupvar, y=variable, fill=value)) +
+  p <- ggplot(na.share, aes(x=!!q.groupvar, y=variable, fill=value)) +
     theme_minimal() +
     geom_tile() +
     scale_fill_viridis(name = "% missing") +
-    theme(axis.text.x = element_text(angle = 45),
+    theme(axis.text.x = element_text(angle = 90),
           axis.ticks.x = element_blank())
   return(p)
 }
